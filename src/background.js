7c10e6c3bd632c4fd3cfc22178ba85507f1ec7ef
Joseph Buchma <josephbuchma@gmail.com>
2016-01-21 17:34:55 +0200

    Fixed bug with editable snippets

--- src/background.js
+++ src/background.js
@@ -2,6 +2,5 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab){
   console.log(tab)
   if (tab.url.indexOf(".slide#") >= 0) {
     chrome.pageAction.show(tab.id);
-    
   }
 });
--- src/initHighlight.js
+++ src/initHighlight.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
   $('div.code pre').each(function(i, block) {
     hljs.highlightBlock(block);
   });
+
+  $('button.run').hover(function(){
+    $('.current div.code').each(function(i, code){
+      var cp;
+      $(code).find('pre.hljs').each(function(i, block){
+        if ($(block).css("display") != "none") {
+          if (typeof(cp) === "undefined" ) {
+            cp = $(block)
+            return true
+          }
+          cp.append("\n"+$(block).html());
+          $(block).remove();
+        }
+      });
+      cp.html(cp.text());
+      hljs.highlightBlock(cp[0]);
+    });
+  });
 });
